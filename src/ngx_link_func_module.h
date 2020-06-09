78f1718304e49e3d8073a88067507af489cfece4
vikaig <vikaig99@gmail.com>
2020-06-09 15:27:21 +0300

    Comment ngx_link_func_ctx_t.uri

--- src/ngx_link_func_module.h
+++ src/ngx_link_func_module.h
@@ -54,7 +54,7 @@ typedef unsigned char u_char;
 #endif
 
 typedef struct {
-    char *uri;
+    char *uri; // Uri
     char *req_args; // Uri Args
     u_char *req_body; // Request Body
     size_t req_body_len; // length of body
